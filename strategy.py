f3bf43c89a35bd2ac7949ff495b36bc5caa2f9c2
LeeKLTW <LeeKLTW@gmail.com>
2019-04-22 21:53:20 +0800

    init tiled, centered, scaled strategy

--- strategy.py
+++ strategy.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+"""
+Because Python has first-class functions, the strategy pattern is unnecessary.
+Knowing the pattern exists can still help us choose a correct design for our program,
+but implement it using a more readable syntax.
+"""
+from PIL import Image
+
+class TiledStrategy:
+    def make_background(self,img_file, desktop_size):
+        pass
+
+class CenteredStrategy:
+    def make_background(self,img_file, desktop_size):
+        pass
+
+class ScaledStrategy:
+    def make_background(self,img_file, desktop_size):
+        pass
