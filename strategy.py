cd75675c715c8109ab8c93b8f550ae9f418dc731
LeeKLTW <LeeKLTW@gmail.com>
2019-04-23 18:58:51 +0800

    __call__, arg, main

--- strategy.py
+++ strategy.py
@@ -19,17 +19,17 @@ class TiledStrategy:
         for x in range(num_tiles[0]):
             for y in range(num_tiles[1]):
                 out_img.paste(im=in_img, box=(
-                in_img.size[0] * x, in_img.size[1] * y, in_img.size[0] * (x + 1), in_img.size[1] * (y + 1)))
+                    in_img.size[0] * x, in_img.size[1] * y, in_img.size[0] * (x + 1), in_img.size[1] * (y + 1)))
         return out_img
 
 
 class CenteredStrategy:
-    def make_background(self, img_file, desktop_size):
+    def __call__(self, img_file, desktop_size):
         in_img = Image.open(fp=img_file)
         out_img = Image.new(model="RGB", size=desktop_size)
-        left = out_img.size[0] - in_img.size[0]//2
-        top = out_img.size[1] - in_img.size[1]//2
-        out_img.paste(im=in_img, box=(left, top, left+in_img.size[0], top+ in_img.size[1]),
+        left = out_img.size[0] - in_img.size[0] // 2
+        top = out_img.size[1] - in_img.size[1] // 2
+        out_img.paste(im=in_img, box=(left, top, left + in_img.size[0], top + in_img.size[1]),
                       )
         return out_img
 
