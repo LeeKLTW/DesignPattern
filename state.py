0fe7196785fb12b20eea08b5e4765542b20e8428
LeeKLTW <LeeKLTW@gmail.com>
2019-04-25 09:44:15 +0800

    done Node

--- state.py
+++ state.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+class Node:
+    def __init__(self, tag_name, parent=None):
+        self.parent = parent
+        self.tag_name = tag_name
+        self.children = []
+        self.text = ""
+    def __str__(self):
+        if self.text:
+            return self.tag_name+':'+ self.text
+        else:
+            return self.tag_name
