d2b305e0224658d0774c506009b3807d6043e505
LeeKLTW <LeeKLTW@gmail.com>
2019-04-26 12:05:24 +0800

    tag

--- state.py
+++ state.py
@@ -5,16 +5,33 @@ class Node:
         self.tag_name = tag_name
         self.children = []
         self.text = ""
+
     def __str__(self):
         if self.text:
-            return self.tag_name+':'+ self.text
+            return self.tag_name + ':' + self.text
         else:
             return self.tag_name
 
+
 class Parser:
-    def __init__(self,parse_string):
+    def __init__(self, parse_string):
         pass
-    def process(self,remaining_string):
+
+    def process(self, remaining_string):
         pass
+
     def start(self):
         pass
+
+class FirstTag:
+    def process(self, remaining_string, parser):
+        pass
+
+class OpenTag:
+    def process(self, remaining_string, parser):
+        pass
+
+class CloseTag:
+    def process(self, remaining_string, parser):
+        pass
+
