bbde98afe623aa3c7f2c0d7be705c9f6fd1328cc
LeeKLTW <LeeKLTW@gmail.com>
2019-04-26 12:42:50 +0800

    name -> tag_name

--- state.py
+++ state.py
@@ -84,7 +84,7 @@ class CloseTag:
         i_end_tag = remaining_string.find(">")
         assert remaining_string[i_start_tag + 1] == '/'
         tag_name = remaining_string[i_start_tag + 2: i_end_tag]
-        assert tag_name == parser.current_node.name
+        assert tag_name == parser.current_node.tag_name
         remaining = remaining_string[i_end_tag + 1:].strip()
 
         parser.current_node = parser.current_node.parent
