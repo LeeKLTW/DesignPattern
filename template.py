23855c0e8d9ab41e4356ed741fafaac58db228e1
LeeKLTW <LeeKLTW@gmail.com>
2019-04-28 23:01:14 +0800

    sample data insert

--- template.py
+++ template.py
@@ -7,3 +7,16 @@ cur = conn.cursor()
 cur.execute(
     "CREATE TABLE Sales (salesperson text, amt currency, year integer, model text, new boolean);")  # SQLite currency
 
+cur.execute("INSERT INTO Sales VALUES ('Tim',16000,2010,'Honda Fit','true')")
+conn.commit()
+cur.execute("INSERT INTO Sales VALUES ('Tim',9000,2006,'Ford Focus','false')")
+conn.commit()
+cur.execute("INSERT INTO Sales VALUES ('Gayle', 8000, 2004, 'Dodge Neon', 'false')")
+conn.commit()
+cur.execute("INSERT INTO Sales VALUES ('Gayle', 28000, 2009, 'Ford Mustang', 'true')")
+conn.commit()
+cur.execute("INSERT INTO Sales VALUES ('Gayle', 50000, 2010, 'Lincoln Navigator', 'true')")
+conn.commit()
+cur.execute("INSERT INTO Sales VALUES ('Don', 20000, 2008, 'Toyota Prius', 'false')")
+conn.commit()
+conn.close()
