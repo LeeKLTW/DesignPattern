b4a1f07fd60bbe513e2f9832f3fe64dbcc2fccb7
LeeKLTW <LeeKLTW@gmail.com>
2019-04-28 22:56:10 +0800

    create table, with currency

--- template.py
+++ template.py
@@ -2,3 +2,8 @@
 import sqlite3
 
 conn = sqlite3.connect('sales.db')
+cur = conn.cursor()
+
+cur.execute(
+    "CREATE TABLE Sales (salesperson text, amt currency, year integer, model text, new boolean);")  # SQLite currency
+
