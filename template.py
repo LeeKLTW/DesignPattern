8a25bbd849346963e49b126e8f77b5ae633edb07
LeeKLTW <LeeKLTW@gmail.com>
2019-04-29 10:56:26 +0800

    template class done

--- template.py
+++ template.py
@@ -23,8 +23,31 @@ def init_db():
     conn.commit()
     conn.close()
 
+
 class QueryTemplate:
-    pass
+    def connect(self):
+        self.conn = sqlite3.connect('sales.db')
+        # self.results, self.query, self.formatted_results
+
+    # todo
+    def construct_query(self):
+        raise NotImplemented
+
+    def do_query(self):
+        cur = self.conn.cursor()
+        results = cur.execute(self.query)
+        self.results = results.fetchall()
+
+    def format_results(self):
+        output = []
+        for row in self.results:
+            row = [str(i) for i in row]
+            output.append(','.join(row))
+        self.formatted_results = '\n'.join(output)
+
+    # todo
+    def output_results(self):
+        raise NotImplemented
 
 
 class NewVehiclesQuery(QueryTemplate):
