dbfd2ec276518f265ab60702978d52ac6e23e793
LeeKLTW <LeeKLTW@gmail.com>
2019-04-29 12:15:16 +0800

    finish age calculator and its adapter

---
+++ adapter.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+class AgeCalculator:
+    def __init__(self, birthday):
+        # birthday = "1970-01-01"
+        self.year, self.month, self.day = (int(i) for i in birthday.split("-"))
+
+    def calculate_age(self, date):
+        year, month, day = (int(i) for i in date.split("-"))
+        age = year - self.year
+        if (month, day) < (self.month, self.day):
+            age -= 1
+        return age
+
+class DateAgeAdapter:
+    def _str_date(self,date):
+        return date.strftime("%Y-%m-%d")
+
+    def __init__(self,birthday):
+        birthday = self._str_date(birthday)
+        self.calculate = AgeCalculator(birthday)
+
+    def get_age(self, date):
+        date = self._str_date(date)
+        return self.calculator.calculate_age(date)
+
+
