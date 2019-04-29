f1c2cfb0b7d5e5471626bc81dcaa194375c69ede
LeeKLTW <LeeKLTW@gmail.com>
2019-04-29 16:33:20 +0800

    1. brief description, 2. file, process server based on abc server, 3. Facade OperatingSystem

--- facade2.py
+++ facade2.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+"""
+An operating system using a multi-server approach, similar to MINIX 3 GNU Hurd.
+A multiserver operating system has a minimal kernel, called the microkernel.
+
+The pros of this approach are that the operating system can become more fault-tolerant, reliable, and secure.
+
+The cons of this approach are the performance overhead and the complexity of system programming,
+because the message passing is more complex than the shared memory model used in monolithic kernels such as Linux.
+"""
+
+from enum import Enum
+from abc import ABCMeta, abstractmethod
+
+State = Enum('State','new running sleeping restart zombie')
+
+class Server(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return self.name
+
+    @abstractmethod
+    def boot(self):
+        pass
+
+    @abstractmethod
+    def kill(self, restart=True):
+        pass
+
+class FileServer(Server):
+    pass
+
+class ProcessServer(Server):
+    pass
+
+class OperatingSystem:
+    """The Facade"""
+    pass
