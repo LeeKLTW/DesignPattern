c06c21102cc549bd0796532d435be243e45c4f6b
LeeKLTW <LeeKLTW@gmail.com>
2019-12-05 16:52:32 +0800

    complete singleton monostate

--- design_pattern/singleton_monostate.py
+++ design_pattern/singleton_monostate.py
@@ -12,6 +12,8 @@ class CEO:
   def __str__(self):
     return f'{self.name} is {self.age} years old.'
 
+
+# How to write it in monostate
 class Monostate:
   _shared_state = {}
 
@@ -19,4 +21,41 @@ class Monostate:
     obj = super(Monostate, cls).__new__(cls,*args,**kwargs)
     obj.__dict__ = cls._shared_state
     return obj
+
+class CFO(Monostate):
+  def __init__(self):
+    self.name = ''
+    self.money_managed = 0
+
+  def __str__(self):
+    return f'{self.name} manages ${self.money_managed}'
+
+
+if __name__ == '__main__':
+  ceo1 = CEO()
+  print(ceo1)
+
+  ceo1.age = 66
+
+  ceo2 = CEO()
+  ceo2.age = 77
+  print(ceo1)
+  print(ceo2)
+
+  ceo2.name = "Tim"
+
+  ceo3 = CEO()
+  print(ceo1, ceo2, ceo3,sep='\n')
+
+  cfo1 = CFO()
+  cfo1.name = 'Sheryl'
+  cfo1.money_managed = 1
+  print(cfo1)
+
+  cfo2 = CFO()
+  cfo2.name = 'Ruth'
+  cfo2.money_managed = 10
+  print(cfo1,cfo2,sep='\n')
+
+
 
