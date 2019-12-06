92e2c9553b9ff2e05100328615d03f6baf925875
LeeKLTW <LeeKLTW@gmail.com>
2019-12-06 12:18:26 +0800

    [WIP] adapter, update rectangle part

--- design_pattern/adapter3.py
+++ design_pattern/adapter3.py
@@ -4,17 +4,20 @@ class Point:
     self.x = x
     self.y = y
 
+
 def draw_point(p):
-  print('.',end='')
+  print('.', end='')
+
 
 class Line:
   def __init__(self, start, end):
     self.start = start
     self.end = end
 
+
 class Rectangle(list):
   def __init__(self, x, y, width, height):
-     super().__init__()
-     self.append()
-
+    super().__init__()
+    self.append(Line(Point(x, y), Point(x + width, y)))
+    self.append(Line(Point(x, y), Point(x + width, y)))
 
