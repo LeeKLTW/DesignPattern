a3c1b89457977a8495d8ed000680b0ad4d03be84
LeeKLTW <LeeKLTW@gmail.com>
2019-12-17 21:54:15 +0800

    complete rectangle

--- design_pattern/adapter4_with_caching.py
+++ design_pattern/adapter4_with_caching.py
@@ -5,6 +5,7 @@ class Point:
     self.x = x
     self.y = y
 
+
 def draw_point(p):
   print('.', end='')
 
@@ -13,4 +14,13 @@ class Line:
   def __init__(self, start, end):
     self.start = start
     self.end = end
+
+
+class Rectangle(list):
+  def __init__(self, x, y, width, height):
+    super().__init__()
+    self.append(Line(Point(x, y), Point(x + width, y)))
+    self.append(Line(Point(x + width, y), Point(x + width, y + height)))
+    self.append(Line(Point(x, y), Point(x, y + height)))
+    self.append(Line(Point(x, y + height), Point(x + width, y + height)))
 
