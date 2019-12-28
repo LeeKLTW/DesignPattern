c17bae8af3f283ed4108c3219564a9ebbc04980f
LeeKLTW <LeeKLTW@gmail.com>
2019-12-28 18:03:42 +0800

    init bridge with inheritance

--- design_pattern/bridge2.py
+++ design_pattern/bridge2.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+class Renderer():
+  def render_circle(self, radius):
+    pass
+
+class VectorRenderer(Renderer):
+  pass
+
+class RasterRender(Renderer):
+  pass
+
+class Shape:
+  pass
+
+class Circle(Shape):
+  pass
