ad88a7b05b80372463c2c31e2585eec5dbf82b99
LeeKLTW <LeeKLTW@gmail.com>
2020-06-16 21:29:08 +0800

    complete bridge3.py

--- design_pattern/bridge3.py
+++ design_pattern/bridge3.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from abc import ABC
+import unittest
+
 class Shape:
     def __init__(self):
         self.name = None
@@ -24,3 +27,36 @@ class VectorSquare(Square):
 class RasterSquare(Square):
     def __str__(self):
         return f'Drawing {self.name} as pixels'
+
+class Renderer(ABC):
+    @property
+    def what_to_render_as(self):
+        return None
+
+class Shape(ABC):
+    def __int__(self, renderer, name):
+        self.renderer = renderer
+        self.name = name
+
+    def __str__(self):
+        return f"Drawing {self.name} as {self.renderer.what_to_render_as}"
+
+
+class Square(Shape):
+    def __init__(self, renderer):
+        super().__init__(renderer, 'Square')
+
+class RasterRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'pixels'
+
+class VectorRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'lines'
+
+class Evalueate(unittest.TestCase):
+    def test_square_vector(self):
+        sq = Square(VectorRenderer())
+        self.assertEqual(str(sq), 'Drawing Square as lines')
